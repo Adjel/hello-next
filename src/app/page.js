c38b12d8f77172fe277afec34f8f7676535614af
Adjel <adjel.touaoula@gmail.com>
2024-04-02 19:12:34 +0200

    correction

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,10 @@ import React from "react";
 import Footer from "./Footer/Footer";
 
 function Home() {
-  const timestamp = new Date().toLocaleString();
   return (
-    <div>
-      <main>
-        <h1>Hello Next!</h1>
-      </main>
-      <footer>Page rendered on {timestamp}</footer>
-    </div>
+    <main>
+      <h1>Hello Next!</h1>
+    </main>
   );
 }
 
